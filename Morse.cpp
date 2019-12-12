b6c1c3280cfcbbc7089c7fa6dbf2fe295e04e206
Bobby Plubell <rlp8xx@mail.umkc.edu>
2019-12-12 13:04:18 -0600

    implementing morse decode and encode

--- Morse.cpp
+++ Morse.cpp
@@ -14,10 +14,12 @@ class MorseMachine {
       // initialize the map
       ifstream morsefile("morse.txt");
       string letter, morsestr;
+      // iterate over every two space or \n delimited words
       while (morsefile >> letter >> morsestr) {
         #ifndef NDEBUG
         cout << "Letter: " << letter << " " << morsestr << endl;
         #endif
+        // insert into both maps
         encode_map.insert(pair<string,string>(letter, morsestr));
         decode_map.insert(pair<string,string>(morsestr, letter));
       }
@@ -51,9 +53,22 @@ class MorseMachine {
       string decoded = "";
       // create a string stream for easily finding space delimited words
       stringstream encoded_stream(encoded);
+
       // iterate over space delimited words in encoded string
       string encoded_char;
-      while (encoded_stream >> encoded_char
+      map<string,string>::iterator it;
+      while (encoded_stream >> encoded_char) {
+        // search map for the encoded char
+        it = this->decode_map.find(encoded_char);
+        if (it != this->decode_map.end()) {
+          // concat results
+          decoded = decoded + it->second;
+        } else {
+          cout << "Invalid morse string found" << endl;
+          return "";
+        }
+      }
+      return decoded;
     }
 
   private:
@@ -65,5 +80,9 @@ class MorseMachine {
 int main() {
   MorseMachine mm = MorseMachine("morse.txt");
   string test_plaintext = "asdf asdf asdf";
-  cout << mm.encode(test_plaintext) << endl;
+  cout << "Test string: " << test_plaintext << endl;
+  string encoded = mm.encode(test_plaintext);
+  cout << "Encoded test string: " << encoded << endl;
+  string decoded = mm.decode(encoded);
+  cout << "Decoded encoded test string: " << decoded << endl;
 }
